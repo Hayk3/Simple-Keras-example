99b7ed142b84c16937df1c3e7c473f5262f94926
Hayk3 <34001550+Hayk3@users.noreply.github.com>
2018-02-08 14:33:27 +0300

    usless change

--- 1. Diabetes.py
+++ 1. Diabetes.py
@@ -9,6 +9,7 @@ dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 # split into input (X) and output (Y) variables
 X = dataset[:,0:8]
 Y = dataset[:,8]
+    
 
 
 # create first model
